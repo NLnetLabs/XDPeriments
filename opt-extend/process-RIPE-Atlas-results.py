0db176d30637946563d180412d206b42810bc03d
Willem Toorop <willem@nlnetlabs.nl>
2021-11-10 10:06:24 +0100

    New measurements using all available probes

--- opt-extend/process-RIPE-Atlas-results.py
+++ opt-extend/process-RIPE-Atlas-results.py
@@ -4,36 +4,40 @@ import requests
 import json
 
 res_url = 'https://atlas.ripe.net/api/v2/measurements/%d/results/?format=json'
-neither = json.loads(requests.get(res_url % 33172615).text)
-eder    = json.loads(requests.get(res_url % 33173068).text)
+eder    = json.loads(requests.get(res_url % 33267733).text)
+neither = json.loads(requests.get(res_url % 33267734).text)
 
-baseline_probes = set([msm['prb_id'] for msm in eder])
-baseline_resolvers = set()
+eder_probes = set([msm['prb_id'] for msm in eder])
 eder_resolvers = set()
+neither_probes = set([msm['prb_id'] for msm in neither])
+neither_resolvers = set()
 
 for msm in neither:
 	if 'resultset' not in msm:
 		continue
 	prb_id = msm['prb_id']
-	if prb_id not in baseline_probes:
+	if prb_id not in eder_probes:
 		continue
 	for rs in msm['resultset']:
 		if 'result' not in rs \
 		or rs['result']['ANCOUNT'] < 1:
 			continue
-		baseline_resolvers.add((prb_id, rs['dst_addr']))
+		neither_resolvers.add((prb_id, rs['dst_addr']))
 
 for msm in eder:
 	if 'resultset' not in msm:
 		continue
 	prb_id = msm['prb_id']
+	if prb_id not in neither_probes:
+		continue
 	for rs in msm['resultset']:
 		if 'result' not in rs \
 		or rs['result']['ANCOUNT'] < 1:
 			continue
 		eder_resolvers.add((prb_id, rs['dst_addr']))
 
-print('# resolvers in both sets: %d' % len(baseline_resolvers & eder_resolvers))
-print('# resolvers only in the neither set: %d' % len(baseline_resolvers - eder_resolvers))
-print('# resolvers only in the eder set: %d' % len(eder_resolvers - baseline_resolvers))
+print('# probes participating: %d' % len(eder_probes & neither_probes))
+print('# resolvers in both sets: %d' % len(eder_resolvers & neither_resolvers))
+print('# resolvers only in the neither set: %d' % len(neither_resolvers - eder_resolvers))
+print('# resolvers only in the eder set: %d' % len(eder_resolvers - neither_resolvers))
 
